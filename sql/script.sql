9f728c6012559e0691e63c595b9f1842a135fb24
calace <felipe.caetano@sptech.school>
2024-11-18 16:20:15 -0300

    adicionando selects.sql

---
+++ sql/script.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE SPTech;
+
+USE SPTech;
+
+CREATE TABLE aluno(
+    RA INT PRIMARY KEY,
+    nome VARCHAR(50),
+);
+
+CREATE TABLE especialidade(
+    fkAluno INT PRIMARY KEY,
+    area VARCHAR(20),
+    nivel INT,
+    CONSTRAINT CHKNivel CHECK(nivel IN('Avançado','Intermediário','Iniciante')),
+    FOREIGN KEY(fkAluno) REFERENCES aluno(RA) 
+);
+CREATE TABLE poder(
+    idPoder INT PRIMARY KEY AUTO_INCREMENT,
+    poder VARCHAR(50)
+);
+CREATE TABLE poderAluno(
+    idPoderAluno int AUTO_INCREMENT,
+    fkPoder int,
+    fkAluno int,
+    FOREIGN KEY(fkAluno) REFERENCES aluno(RA),
+    FOREIGN KEY(fkPoder) REFERENCES poder(idPoder),
+    PRIMARY KEY(idPoderAluno, fkAluno, fkPoder)
+);
+
---
+++ sql/selects.sql
@@ -0,0 +1,7 @@
+/*
+1. Mostre todos os alunos que tem o poder relacionado a "Força" (Facil)
+*/
+
+/*
+2.
+*/
