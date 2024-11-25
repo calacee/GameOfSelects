765b664552331bcd72dd4f72e7555a65c33fcfd0
mateussroque <mateus.pereira@sptech.school>
2024-11-25 12:18:33 -0300

    Update script.sql

--- sql/script.sql
+++ sql/script.sql
@@ -1,4 +1,4 @@
-
+	
 CREATE DATABASE SPTech;
 
 USE SPTech;
@@ -6,7 +6,7 @@ USE SPTech;
 CREATE TABLE aluno(
     RA INT PRIMARY KEY,
     nome VARCHAR(50),
-    fkMonitor int,
+    fkMonitor INT,
     FOREIGN KEY(fkMonitor) REFERENCES aluno(RA)
 );
 INSERT INTO aluno (RA, nome) VALUES
@@ -27,23 +27,21 @@ INSERT INTO poder (poder) VALUES
 ('Telepatia'),
 ('Velocidade Sobrenatural');
 
-
 CREATE TABLE poderAluno(
-    idPoderAluno int AUTO_INCREMENT,
-    QtdPoder float,
-    fkPoder int,
-    fkAluno int,
+    idPoderAluno INT AUTO_INCREMENT,
+    QtdPoder FLOAT,
+    fkPoder INT,
+    fkAluno INT,
     FOREIGN KEY(fkAluno) REFERENCES aluno(RA),
     FOREIGN KEY(fkPoder) REFERENCES poder(idPoder),
     PRIMARY KEY(idPoderAluno, fkAluno, fkPoder)
 );
 INSERT INTO poderAluno (QtdPoder, fkPoder, fkAluno) VALUES
-(100,1, 01242074), -- Luz: Invisibilidade
-(100,2, 01242049), -- Luiza: Super Força
-(100,3, 01242137), -- Paloma: Controle de Tecnologia
-(100,4, 01242061), -- Calace: Telepatia
-(100,5, 01242015); -- Mateus: Velocidade Sobrenatural
-
+(100, 1, 01242074), -- Luz: Invisibilidade
+(100, 2, 01242049), -- Luiza: Super Força
+(100, 3, 01242137), -- Paloma: Controle de Tecnologia
+(100, 4, 01242061), -- Calace: Telepatia
+(100, 5, 01242015); -- Mateus: Velocidade Sobrenatural
 
 CREATE TABLE vilao (
     idVilao INT PRIMARY KEY AUTO_INCREMENT,
@@ -51,11 +49,11 @@ CREATE TABLE vilao (
     caracteristica VARCHAR(100)
 );
 INSERT INTO vilao (nome, caracteristica) VALUES
-('Dr. Malware', 'Controla vírus digitais'),
-('Shadow', 'Manipula as sombras'),
-('Capitão Bug', 'Causa erros e sabota sistemas'),
-('Trojanizer', 'Mestre da infiltração furtiva'),
-('Overclock', 'Extremamente rápido, mas instável');
+('Dr. Malware', 'Controle de Tecnologia'),  -- Correspondente à fraqueza e poder do aluno
+('Shadow', 'Invisibilidade'),               -- Correspondente à fraqueza e poder do aluno
+('Capitão Bug', 'Velocidade Sobrenatural'), -- Correspondente à fraqueza e poder do aluno
+('Trojanizer', 'Telepatia'),                -- Correspondente à fraqueza e poder do aluno
+('Overclock', 'Super Força');               -- Correspondente à fraqueza e poder do aluno
 
 CREATE TABLE fraqueza (
     idFraqueza INT PRIMARY KEY AUTO_INCREMENT,
@@ -68,7 +66,6 @@ INSERT INTO fraqueza (fraqueza) VALUES
 ('Telepatia'),              -- Fraqueza de Trojanizer
 ('Super Força');            -- Fraqueza de Overclock
 
-
 CREATE TABLE vilaoDoAluno (
     fkVilao INT,
     fkAluno INT,
@@ -79,10 +76,8 @@ CREATE TABLE vilaoDoAluno (
 INSERT INTO vilaoDoAluno (fkVilao, fkAluno) VALUES
 (1, 01242137), -- Dr. Malware enfrenta Paloma (Controle de Tecnologia)
 (2, 01242074), -- Shadow enfrenta Luz (Invisibilidade)
-(3, 01242015), -- Captain Bug enfrenta Mateus (Velocidade Sobrenatural)
+(3, 01242015), -- Capitão Bug enfrenta Mateus (Velocidade Sobrenatural)
 (4, 01242061), -- Trojanizer enfrenta Calace (Telepatia)
 (5, 01242049); -- Overclock enfrenta Luiza (Super Força)
-
-    
 
 
